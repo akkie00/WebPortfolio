96deca2a2aecafad8e6270bab6ff85ebf7d177d7
akkie00 <akshay8801.ak@gmail.com>
2018-05-07 09:24:15 +0530

    added toggle function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Portfolio } from './../models/portfolio.interface';
 })
 export class AppComponent implements OnInit{
   public Portfolio: Portfolio;
+  public logOpen: boolean = true; 
   public ngOnInit(): void{
     this.Portfolio= {
       firstName: "Akshay",
@@ -26,11 +27,62 @@ export class AppComponent implements OnInit{
           icon: 'fa fa-github fa-2x',
           link: 'https://github.com/akkie00',
           color: '#6e5494'
+        },
+        {
+          icon: 'fa fa-twitter-square fa-2x',
+          link: 'https://twitter.com/akkie2192',
+          color: '#00aced'
+        },
+        {
+          icon: 'fa fa-qoura fa-2x',
+          link:  'https://www.quora.com/profile/Akshay-Kumar-693',
+          color: '#D01010'
         }
       ],
       introduction: "Hello there, my name is Akshay kumar and I've started learning web development using HTML, CSS, JavaScript. This is my first Angular Project.",
-      changeLog: [],
-      updates: [],
+      changeLog: [
+        {
+          color: '#171616',
+          month: 'April',
+          year: 2018,
+          highlight: 'Completed Udacity Free Scholorship course for Front-end Web Development',
+          details: `Completed the course from Udacity which started on February, 2018. It was 
+          a good starter course for beginners with things like HTML, CSS, JavaScript, JQuery and
+          a small project.`
+        },
+        {
+          color: '#171616',
+          month: 'February',
+          year: 2018,
+          highlight: 'Created git hub profile',
+          details: 'Started working on Git and pushing changes to it more frequently since then'
+        },
+        {
+          color: '#504848',
+          month: 'August',
+          year: 2017,
+          highlight: 'Started working on my DevOps project in my company.',
+          details: `The project which I work on is basically is a support project,
+          in which we have to setup environment and provide it to the developers.
+          It deals with problems that developers and testers face while they're working. 
+          We have to make sure that services are not interrupted and that the workflow is 
+          smooth.`
+        }
+      ],
+      updates: [
+        {
+          description: 'Medium',
+          link: 'https://medium.com/@akshaykumar_92451/has-recommended'
+        },
+        {
+          description: 'Quora Answers',
+          link: 'https://www.quora.com/profile/Akshay-Kumar-693'
+        },
+        {
+          description: 'Udacity\'s PixelArtMaker Project',
+          link: 'https://github.com/akkie00/UdacityProjects/tree/master/PixelArt'
+        }
+      ],
       speakerBio: `Hi, I'm Akshay and I've recently started learning web development and
       I'm currently learning HTML, CSS, JavaScript and Angular. I hope to build a complete 
       web application on my own by the end of this year both front and back end. Apart from 
@@ -38,6 +90,12 @@ export class AppComponent implements OnInit{
       user on Medium and hoping to write my very own article after completing my first project.
       This bio will be a very basic one since I've just started learning. And I hope this will 
       change at the end of the year.`
+    }
+  }
+  public toggleOpen(valueToToggle: string): void{
+    switch(valueToToggle){
+      case 'logOpen':
+        this.logOpen = !this.logOpen;
     }
   }
 }
